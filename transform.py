8f7f05f95ad35ede38aeee0554fe59fe5041c653
Krishnan Ramaswamy <krramasw@cisco.com>
2016-11-03 16:09:28 -0700

    customer analytics.pptx

--- transform.py
+++ transform.py
@@ -17,11 +17,35 @@ def updateDistinct(masterMetaDict, customerLevermMetaDict,partner,item) :
     else:
         masterMetaDict[item] = 1
 
-def dump(dictionary) :
+
+def updateMasterCompanies(masterCompanies,partner) :
+    if partner in masterCompanies:
+        masterCompanies[partner] = masterCompanies[partner] + 1
+    else:
+        masterCompanies[partner] = 1
+
+
+def dump(partner,dictionary,f) :
+
+    tmpDict = dict()
 
     for key in sorted(dictionary):
-        print "%s: %s" % (key, dictionary[key])
+        #print "%s: %s" % (key, dictionary[key])
+        partner_value = dictionary[key]
 
+        tmparray = key.split("-")
+
+        #print "JJJJJJ " + tmparray[0] + "  KKKK" + str(partner_value)
+        if tmparray[0] == partner :
+
+            if partner in tmpDict:
+                tmpDict[partner] = tmpDict[partner] + "," + tmparray[1]
+            else:
+                tmpDict[partner] =  tmparray[1]
+
+    for key in tmpDict :
+        f.write("%s: %s" % (key, tmpDict[key]))
+        print " HERE you go" + "%s: %s" % (key, tmpDict[key])
 
 
 if __name__ == '__main__':
@@ -29,17 +53,18 @@ if __name__ == '__main__':
     masterProducts = dict()
     masterVersion = dict()
     masterOs = dict()
+    masterCompanies = dict()
 
     distinctProducts = dict()
     distinctVersion  = dict()
     distinctOs       = dict()
 
-    data = pd.read_csv("export_psirt.csv")
+    data = pd.read_csv("../export_psirt.csv")
 
     import csv
     import sys
 
-    f = open("export_psirt.csv", 'r')
+    f = open("../export_psirt.csv", 'r')
     try:
         reader = csv.reader(f)
         for row in reader:
@@ -47,23 +72,42 @@ if __name__ == '__main__':
             updateDistinct(masterProducts,distinctProducts,row[0],row[2])
             updateDistinct(masterVersion,distinctVersion,row[0], row[3])
             updateDistinct(masterOs,distinctOs,row[0], row[4])
+            updateMasterCompanies(masterCompanies,row[0])
     finally:
         f.close()
 
-    print "************************** Master Products *******************************"
-    dump(masterProducts)
+    print "************************** Master Companies *******************************"
 
-    print "************************** Master Version *******************************"
-    dump(masterVersion)
+    for partner in sorted(masterCompanies):
 
-    print "************************** Master Os *******************************"
-    dump(masterOs)
+        print "%s: %s" % (partner, masterCompanies[partner])
 
-    print "************************** Distinct Products *******************************"
-    dump(distinctProducts)
+        print "************************** Master Products *******************************"
+        f = open("../product_master.csv", 'w')
+        dump(partner,masterProducts,f)
+        f.close()
 
-    print "************************** Distinct Products *******************************"
-    dump(distinctVersion)
+        print "************************** Master Version *******************************"
+        f = open("../version_master.csv", 'w')
+        dump(partner,masterVersion,f)
+        f.close()
 
-    print "************************** Distinct Products *******************************"
-    dump(distinctOs)
+        print "************************** Master Os *******************************"
+        f = open("../os_master.csv", 'w')
+        dump(partner,masterOs,f)
+        f.close()
+
+        print "************************** Distinct Products *******************************"
+        f = open("../product_vector.csv", 'w')
+        dump(partner,distinctProducts,f)
+        f.close()
+
+        print "************************** Distinct Products *******************************"
+        f = open("../version_vector.csv", 'w')
+        dump(partner,distinctVersion,f)
+        f.close()
+
+        print "************************** Distinct Products *******************************"
+        f = open("../os_vector.csv", 'w')
+        dump(partner,distinctOs,f)
+        f.close()
