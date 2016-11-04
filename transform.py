a7a91bd33b9d749822aa7ddb031386133f2c43d7
Krishnan Ramaswamy <krramasw@cisco.com>
2016-11-03 20:57:07 -0700

    customer analytics.pptx

--- transform.py
+++ transform.py
@@ -6,7 +6,7 @@ from IPython.display import display # Allows the use of display() for DataFrames
 
 def updateDistinct(masterMetaDict, customerLevermMetaDict,partner,item) :
 
-    key = partner+"-"+item
+    key = partner+":"+item
     if key in customerLevermMetaDict :
         customerLevermMetaDict[key] = customerLevermMetaDict[key] + 1
     else :
@@ -25,25 +25,43 @@ def updateMasterCompanies(masterCompanies,partner) :
         masterCompanies[partner] = 1
 
 
-def dump(partner,dictionary,f) :
+def dumpHeader(keyStr,masterDict,f) :
+
+    f.write(keyStr + ",")
+    for key in sorted(masterDict):
+        if key == None or key == "" : continue
+        f.write(key + ",")
+
+    f.write("\n")
+
+
+def dump(partner,masterDict,dictionary,f) :
 
     tmpDict = dict()
 
-    for key in sorted(dictionary):
+
+    tmpDict[partner] = ""
+
+    for key in sorted(masterDict) :
+
         #print "%s: %s" % (key, dictionary[key])
-        partner_value = dictionary[key]
 
-        tmparray = key.split("-")
+        if partner+":"+key in dictionary :
 
-        if tmparray[0] == partner :
-
+            value = dictionary[partner+":"+key]
             if partner in tmpDict:
-                tmpDict[partner] = tmpDict[partner] + "," + tmparray[1]
+                tmpDict[partner] = str(tmpDict[partner]) + "," + str(value)
             else:
-                tmpDict[partner] =  tmparray[1]
+                tmpDict[partner] =  value
+
+        else :
+            tmpDict[partner] = str(tmpDict[partner]) + ","
+
+
 
     for key in tmpDict :
         f.write("%s: %s" % (key, tmpDict[key]))
+        f.write("\n")
         print " HERE you go" + "%s: %s" % (key, tmpDict[key])
 
 
@@ -69,22 +87,41 @@ if __name__ == '__main__':
         for row in reader:
             #print row
             updateDistinct(masterProducts,distinctProducts,row[0],row[2])
-            updateDistinct(masterVersion,distinctVersion,row[0], row[3])
-            updateDistinct(masterOs,distinctOs,row[0], row[4])
+            updateDistinct(masterOs, distinctOs, row[0], row[3])
+            updateDistinct(masterVersion,distinctVersion,row[0], row[4])
             updateMasterCompanies(masterCompanies,row[0])
     finally:
         f.close()
 
     print "************************** Master Companies *******************************"
 
+    f = open("../product_vector.csv", 'w')
+    dumpHeader("PARTNER",masterProducts, f)
     for partner in sorted(masterCompanies):
 
         print "%s: %s" % (partner, masterCompanies[partner])
-
         print "************************** Distinct Products *******************************"
-        f = open("../product_vector.csv", 'w')
-        dump(partner, distinctProducts, f)
-        f.close()
+        dump(partner, masterProducts,distinctProducts, f)
+
+    f.close()
+
+    f = open("../version_vector.csv", 'w')
+    dumpHeader("PARTNER", masterVersion, f)
+    for partner in sorted(masterCompanies):
+        print "%s: %s" % (partner, masterCompanies[partner])
+        print "************************** Distinct Products *******************************"
+        dump(partner, masterVersion, distinctVersion, f)
+
+    f.close()
+
+    f = open("../os_vector.csv", 'w')
+    dumpHeader("PARTNER", masterOs, f)
+    for partner in sorted(masterCompanies):
+        print "%s: %s" % (partner, masterCompanies[partner])
+        print "************************** Distinct Products *******************************"
+        dump(partner, masterOs, distinctVersion, f)
+
+    f.close()
 
 
 if False :
